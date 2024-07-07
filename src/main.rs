c8808fc5f4a9269e83124ffefbb3c95f2eb9cc18
isabel <isabel@isabelroses.com>
2024-07-07 09:51:22 +0100

    feat: add clap and basic command structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{CommandFactory, Parser};
+use clap_complete::generate;
+
+mod commands;
+use commands::{Cli, Commands};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Some(Commands::Completions { shell }) => {
+            let mut cmd = Cli::command();
+            let name = cmd.get_name().to_string();
+            eprintln!("Generating completions for {shell}");
+            generate(shell, &mut cmd, name, &mut std::io::stdout());
+            std::process::exit(0);
+        }
+        _ => {
+            println!("Hello, world!");
+        }
+    }
 }
