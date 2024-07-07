436950d0a86cbf7af62e1a2facacfbbef53cffe7
robin <67917529+comfysage@users.noreply.github.com>
2024-07-07 10:32:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Astral
+is a command line interface tool designed to interact with running Neovim instances, allowing users to list active instances and open files in a specified instance from different shells.
---
+++ default.nix
@@ -0,0 +1,28 @@
+{ lib, rustPlatform }:
+let
+  toml = (lib.importTOML ./Cargo.toml).package;
+in
+rustPlatform.buildRustPackage {
+  pname = toml.name;
+  inherit (toml) version;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.intersection (lib.fileset.fromSource (lib.sources.cleanSource ./.)) (
+      lib.fileset.unions [
+        ./Cargo.toml
+        ./Cargo.lock
+        ./src
+      ]
+    );
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  meta = {
+    inherit (toml) homepage description;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ comfysage ];
+    mainPackage = toml.name;
+  };
+}
